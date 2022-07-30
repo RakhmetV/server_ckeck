fe06a7a65a93f3df6fad7867beb13e90f7d5edac
RakhmetV <108589498+RakhmetV@users.noreply.github.com>
2022-07-31 00:44:03 +0500

    second adm and user

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -5,8 +5,8 @@ from tgbot.keyboards.inline import language_keyboard
 
 
 async def admin_start(message: Message):
-    #await  message.answer('Привет! выбери язык/Hi! choose a language', parse_mode='HTML', reply_markup=language_keyboard)
-    await  message.answer('Привет! выбери язык/Hi! choose a language', parse_mode='HTML')
+    await  message.answer('Привет! выбери язык/Hi! choose a language', parse_mode='HTML', reply_markup=language_keyboard)
+    #await  message.answer('Привет! выбери язык/Hi! choose a language', parse_mode='HTML')
     #await message.reply("Hello, admin!")
 
 
